9e3c76eafefb5ed37c7b35bb30c7be0d1e3bb270
Newton Migosi <newtonmigosi@gmail.com>
2018-04-17 11:21:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
 
 function deleteFromObjectByKey (object, key) {
   var temp = Object.assign({}, object);
-  delete temp[object];
+  delete temp[key];
   return temp;
 }
 
